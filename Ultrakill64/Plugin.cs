78e400b61460e48bc30c241452cd7dccf5298df7
Hydraxous <robo-trey@outlook.com>
2024-04-01 21:52:47 -0700

    initial commit

---
+++ Ultrakill64/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Ultrakill64
+{
+    [HarmonyPatch]
+    [BepInPlugin(ConstInfo.GUID, ConstInfo.NAME, ConstInfo.VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony;
+
+        private static ConfigEntry<bool> enableFiltering;
+        public const string NAME = "Ultrakill64";
+        public const string VERSION = "1.0.0";
+        public const string GUID = "Hydraxous.ULTRAKILL.Ultrakill64";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            harmony = new Harmony(ConstInfo.GUID);
+            harmony.PatchAll();
+            
+            enableFiltering = Config.Bind("General", "Enabled", true, "Enable N64 texture filtering");
+            enableFiltering.SettingChanged += (_,_) => SetEffect(enableFiltering.Value);
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(PostProcessV2_Handler), nameof(PostProcessV2_Handler.Fooled)), HarmonyPrefix]
+        private static bool OnFooled(PostProcessV2_Handler __instance)
+        {
+            SetEffect(enableFiltering.Value);
+            return false;
+        }
+
+        private static void SetEffect(bool enabled)
+        {
+            if (enabled)
+                Shader.EnableKeyword("Fooled");
+            else
+                Shader.DisableKeyword("Fooled");
+        }
+
+    }
+}
